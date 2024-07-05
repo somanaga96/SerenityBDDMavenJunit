0398fbd33e305f5d0f098dfce65d24130aa5c040
somapmk96 <somapmk96@gmail.com>
2024-07-05 19:22:50 +0530

    testsuite updated with cricbuzz

--- src/test/java/CucumberTestSuite.java
+++ src/test/java/CucumberTestSuite.java
@@ -6,7 +6,8 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
-@IncludeTags({"green", "red","cricbuzz"})
-//@IncludeTags({"red"})
+//@IncludeTags({"green", "red","cricbuzz"})
+@IncludeTags({"cricbuzz"})
 public class CucumberTestSuite {
 }
+
